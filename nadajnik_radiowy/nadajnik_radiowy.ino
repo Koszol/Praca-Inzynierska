92120f76d64a1114f42d2f8ca74aa90e9f617784
Koszol <bartosz.koszolko@gmail.com>
2022-12-03 18:02:17 +0100

    Dziala git

--- nadajnik_radiowy/nadajnik_radiowy.ino
+++ nadajnik_radiowy/nadajnik_radiowy.ino
@@ -2,13 +2,13 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-const int CE=9;
-const int CSN=10;
-RF24 radio(CE,CSN); //CE,CSN
-float payload=0.1;
+const int CE = 9;
+const int CSN = 10;
+RF24 radio(CE, CSN); //CE,CSN
+float payload = 0.1;
 float text;
-const byte address[][6]={"00001","00002"};
-const int us_sig=8;
+const byte address[][6] = {"00001", "00002"};
+const int us_sig = 8;
 
 void setup() {
   Serial.begin(9600);
@@ -17,22 +17,27 @@ void setup() {
   radio.openWritingPipe(address[1]);
   radio.openReadingPipe(1, address[0]);
   radio.setPALevel(RF24_PA_MIN);
+  pinMode(us_sig, OUTPUT);
 }
 
 void loop() {
-  unsigned long start_timer=micros();
   radio.stopListening();
-  bool report=radio.write(&payload,sizeof(float));
-  unsigned long end_timer = micros();  
+  unsigned long start_timer = micros();
+  radio.write(&payload, sizeof(float));
+  unsigned long end_timer = micros();
+  delayMicroseconds(68);
+  sendUS();
   Serial.print("Time: ");
-  Serial.print(end_timer-start_timer);
+  Serial.print(end_timer - start_timer);
   Serial.print("us. Sent:");
   Serial.println(payload);
-  delay(5);
-  radio.startListening();
-  while(!radio.available());
-  radio.read(&text,sizeof(text));
-  Serial.print("Received:");
-  Serial.println(text);
+
+
   delay(1000);
+}
+
+void sendUS() {
+  digitalWrite(us_sig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(us_sig, LOW);
 }
--- odbiornik_radiowy/odbiornik_radiowy.ino
+++ odbiornik_radiowy/odbiornik_radiowy.ino
@@ -8,7 +8,10 @@ RF24 radio(CE, CSN); //CE,CSN
 float payload;
 float text = 0.2;
 const byte address[][6] = {"00001", "00002"};
-const int us_sig = 8;
+const int fake_sig = 8;
+const int echo = 7;
+long czas;
+float distance;
 
 void setup() {
   Serial.begin(9600);
@@ -17,22 +20,31 @@ void setup() {
   radio.openWritingPipe(address[0]);
   radio.openReadingPipe(1, address[1]);
   radio.setPALevel(RF24_PA_MIN);
+  pinMode(fake_sig,OUTPUT);
+  pinMode(echo,INPUT);
 }
 
 void loop() {
   radio.startListening();
   while (!radio.available());
-  radio.read(&payload, sizeof(payload));
-  Serial.print("Received:");
-  Serial.println(payload);
-  delay(5);
-  radio.stopListening();
   unsigned long start_timer = micros();
-  bool report=radio.write(&text,sizeof(float));
+  radio.read(&payload, sizeof(payload));
   unsigned long end_timer = micros();
+  readUS();
+  /*Serial.print("Received:");
+  Serial.println(payload);
   Serial.print("Time: ");
-  Serial.print(end_timer-start_timer);
-  Serial.print("us. Sent:");
-  Serial.println(text);  
-  delay(1000);
+  Serial.print(end_timer - start_timer);
+  Serial.print("us.");
+  */
+  //delay(500);
+}
+
+void readUS() {
+  digitalWrite(fake_sig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(fake_sig, LOW);
+  czas = pulseIn(echo, HIGH);
+  distance=0.0344*czas;
+  Serial.println(distance-962+25);
 }
